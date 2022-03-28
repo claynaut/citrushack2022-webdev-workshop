c636da6a0e1efb9faf3a8c181d062bfba89233da
claynaut <hello@jspescas.io>
2022-03-28 13:29:26 -0700

    Implement deleting tasks

--- components/TodoList/TodoList.js
+++ components/TodoList/TodoList.js
@@ -3,6 +3,7 @@ import { BiPlus, BiTrash, BiEdit } from 'react-icons/bi'
 import { Accordion } from '../Accordion';
 import { Task } from './Task';
 import styles from '../../styles/TodoList.module.css'
+import { nanoid } from 'nanoid';
 
 const EndOptions = ({ editAction, deleteAction }) => (
   <span className={styles.options} onClick={() => editAction()}>
@@ -17,11 +18,11 @@ const EndOptions = ({ editAction, deleteAction }) => (
 
 export function TodoList({ id, list, lists, setLists }) {
   const { listName, tasks } = list
+  const listIdx = lists.findIndex((list) => list.id === id)
   const [editable, setEditable] = useState(false)
   const [newName, setNewName] = useState(listName)
 
   const toggleComplete = (taskId) => {
-    const listIdx = lists.findIndex((list) => list.id === id)
     let updatedList = {...list}
     let taskToUpdate = updatedList.tasks.find((task) => task.id === taskId)
     taskToUpdate.complete = !taskToUpdate.complete
@@ -36,8 +37,6 @@ export function TodoList({ id, list, lists, setLists }) {
   }
 
   const deleteList = () => {
-    const listIdx = lists.findIndex((list) => list.id === id)
-
     setLists(prevLists => {
       return [
         ...prevLists.slice(0, listIdx),
@@ -53,7 +52,6 @@ export function TodoList({ id, list, lists, setLists }) {
 
   const editListName = (e) => {
     if (e.key === 'Enter') {
-      const listIdx = lists.findIndex((list) => list.id === id)
       let updatedList = {...list}
       updatedList.listName = newName
       console.log(updatedList)
@@ -67,6 +65,40 @@ export function TodoList({ id, list, lists, setLists }) {
       })
       setEditable(false)
     }
+  }
+
+  const createTask = () => {
+    const newTask = {
+      id: nanoid(),
+      taskName: 'Lorem ipsum',
+      complete: false
+    }
+    let updatedList = {...list}
+    updatedList.tasks.push(newTask)
+    setLists(prevLists => {
+      return [
+        ...prevLists.slice(0, listIdx),
+        updatedList,
+        ...prevLists.slice(listIdx + 1)
+      ]
+    })
+  }
+
+  const deleteTask = (taskId) => {
+    let updatedList = {...list}
+    const taskIdx = updatedList.tasks.findIndex((task) => task.id === taskId)
+    updatedList.tasks = [
+      ...(updatedList.tasks).slice(0, taskIdx),
+      ...(updatedList.tasks).slice(taskIdx + 1)
+    ]
+
+    setLists(prevLists => {
+      return [
+        ...prevLists.slice(0, listIdx),
+        updatedList,
+        ...prevLists.slice(listIdx + 1)
+      ]
+    })
   }
 
   return (
@@ -96,12 +128,14 @@ export function TodoList({ id, list, lists, setLists }) {
             toggleComplete={toggleComplete}
             endOptions={
               <EndOptions
+                editAction={() => {}}
+                deleteAction={() => deleteTask(id)}
               />
             }
           />
         )}
         <ul>
-          <button className={styles.newTaskButton}>
+          <button className={styles.newTaskButton} onClick={() => createTask()}>
             <BiPlus /> Add new task
           </button>
         </ul>
